80787f19d4b5c85600ea68f1adc9e747151fe8fe
rngsurrounded <kircore970@gmail.com>
2025-03-20 11:11:29 +0900

    build: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', 
+    emptyOutDir: true, 
+  },
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    tailwindcss()
+  ],
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+  publicDir: 'public',
+});
